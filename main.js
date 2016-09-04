0a130f2a2c0f9fbb2d55459d18ca915f3bd06969
Dazai <smallcute902@gmail.com>
2016-09-04 11:58:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ var ctx = canvas.getContext("2d");
 
 function draw(){
 ctx.drawImage(bgImg,0,0);
-ctx.drawImage(enemyImg,0,0);
+ctx.drawImage(enemy.x,enemy.y);
 }
 
 var enemyImg = document.createElement("img");
 enemyImg.src = "images/slime.gif";
+var enemy = {
+  x:0
+  y:0
+};
 
 setTimeout(draw,1000);
