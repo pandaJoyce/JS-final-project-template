ba602dd2b01e901e206c6e6b73a9d1927d574efd
Dazai <smallcute902@gmail.com>
2016-09-04 11:44:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
+
 function draw(){
 ctx.drawImage(bgImg,0,0);
 }
+
+var enemyImg = document.createElement("img");
+enemyImg.src = "images/slime.gif";
+
 setTimeout(draw,1000);
