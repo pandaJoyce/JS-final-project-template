0ec06c176077cadb259f1a23a9a0ff215c41c950
Dazai <smallcute902@gmail.com>
2016-09-04 12:00:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,17 @@ bgImg.src = "images/map.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
-function draw(){
-ctx.drawImage(bgImg,0,0);
-ctx.drawImage(enemy.x,enemy.y);
-}
-
 var enemyImg = document.createElement("img");
 enemyImg.src = "images/slime.gif";
+
 var enemy = {
   x:0
   y:0
 };
+
+function draw(){
+ctx.drawImage(bgImg,0,0);
+ctx.drawImage(enemy.x,enemy.y);
+}
 
 setTimeout(draw,1000);
