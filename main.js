14e7c661279d2f51718c836644da57201f2814c2
Dazai <smallcute902@gmail.com>
2016-09-11 11:04:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ towbtnImg.src = "images/tower-btn.png";
 function draw(){
 ctx.drawImage(bgImg,0,0);
 ctx.drawImage(enemyImg,enemy.x,enemy.y);
-ctx.drawImage(towbtnImg,608,448,50,50);
+ctx.drawImage(towbtnImg,550,400,50,50);
 }
 
 setInterval(draw,16);
