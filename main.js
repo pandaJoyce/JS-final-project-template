4299b462eeb80edf1ab7017f060421574bdf3034
Dazai <smallcute902@gmail.com>
2016-09-11 11:28:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -14,10 +14,18 @@ var enemy = {
 var towbtnImg = document.createElement("img");
 towbtnImg.src = "images/tower-btn.png";
 
+var towImg = document.creatElement("img")
+towImg.src = "images/tower.png";
+$("canvas").mousemove(mouse(){
+  var cursor = {x:event.offsetX,y:event.offsetY};
+})
+
+
 function draw(){
 ctx.drawImage(bgImg,0,0);
 ctx.drawImage(enemyImg,enemy.x,enemy.y);
 ctx.drawImage(towbtnImg,640-32,480-32,32,32);
+ctx.drawImage(towImg,cursor.x,cursor.y);
 }
 
 setInterval(draw,16);
